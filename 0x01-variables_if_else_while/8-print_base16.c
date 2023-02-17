22d19234872372847a33279bbb5f9d06ae8f13ae
Davis198 <makoshie@gmail.com>
2023-02-17 12:01:01 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,14 @@ int main(void)
 {
 	int num;
 	char la;
-	
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	for (num = 'a'; la <= 'f'; la++)
-		putchar(la); 
-	
+		putchar(la);
 
 	putchar('\n');
-		
+
 	return (0);
 }

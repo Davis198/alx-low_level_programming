e04fdf23a8304053beabca3c0d3c2a08e23fef42
Davis198 <makoshie@gmail.com>
2023-02-17 20:59:43 -0800

    combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,49 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - Prints all combination of sigle digits
+ * main - Prints all combination of single digits
  *
  * Return: Always 0
  */
 int main(void)
 {
-        int i, j, k;
-    for (i = 0; i < 10; i++) 
-    {
-        putchar(i + '0');
-        if (i < 9) 
+                int n = 0;
+
+        while (n < 10)
 	{
-            putchar(',');
-            putchar(' ');
-	}
-        for (j = i + 1; j < 10; j++) 
-	{
-            putchar(i + '0');
-            putchar(',');
-            putchar(' ');
-            putchar(j + '0');
-            if (j < 9) 
-	    {
-                putchar(',');
-                putchar(' ');
-            }
-            for (k = j + 1; k < 10; k++) 
-	    {
-                putchar(i + '0');
-                putchar(',');
-                putchar(' ');
-                putchar(j + '0');
-                putchar(',');
-                putchar(' ');
-                putchar(k + '0');
-                if (k < 9) 
-		{
-                    putchar(',');
-                    putchar(' ');
+                putchar(48 + n);
+                if (n != 9)
+                {
+                         putchar (',');
+                         putchar (' ');
                 }
-            }
-        }
-    }
+                n++;
+	}
+
+        putchar('\n');
 
 	return (0);
 }

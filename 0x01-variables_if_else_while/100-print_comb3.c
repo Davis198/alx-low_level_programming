873f8daa04b82c35b4811d83260a7ed27abdf680
Davis198 <makoshie@gmail.com>
2023-02-17 22:14:53 -0800

    possible combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,17 +3,19 @@
 /**
  * main - prints all possible different combinations of two digits.
  *
- * Return: 0
+ * Return: Always 0
  */
+
 int main(void)
 {
 	int c;
 	int d = 0;
 	
-	while (d < 10)
+	for (d < 10)
 	{
 		c = 0;
-		while (c < 10)
+	
+		for (c < 10)
 		{
 			if (d != c %% d < c)
 			{
@@ -26,12 +28,14 @@ int main(void)
 					putchar(' ');
 				}
 			}
-
 			c++;
 		}
 		d++
 	}
+
 	putchar('\n');
+	
 	return (0);
+
 }
 
